fa7d1dafb598015042f8bf51e8ce6ebf6151d8fd
Siva Kumar <mariyalasivakumar@gmail.com>
2025-06-24 10:40:37 +0530

    added activitystatus

--- backend/server.js
+++ backend/server.js
@@ -1272,10 +1272,12 @@ app.post('/verify-otp', (req, res) => {
 });
 
 app.post('/forgot-password/send-otp', (req, res) => {
+  console.log('Received forgot-password/send-otp request');
+  console.log('Request body:', req.body);
   const { mobile } = req.body;
   if (!mobile) return res.status(400).json({ success: false, message: 'Mobile required' });
   console.log('Received mobile:', mobile);
-  db.query('SELECT * FROM users WHERE mobile = ? ', [mobile], (err, results) => {
+  db.query('SELECT * FROM users WHERE mobile = ? AND activity_status = "active"', [mobile], (err, results) => {
     console.log('Results:', err);
     if (err) return res.status(500).json({ success: false, message: 'Database error' });
     console.log('Results:', results);
